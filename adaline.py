bc34fa232105c482d129edb6b9627ec75ae115e7
Guilherme Alves <guilherme.alves3340@gmail.com>
2018-09-21 10:32:38 -0300

    Correções significativas

--- adaline.py
+++ adaline.py
@@ -12,10 +12,10 @@ x = [[1,1],
     [1,1],
     [1,-1],
     [-1,-1]]
-
+matriz = [[],[]]
 # Parametros
 t = [1,-1, -1, -1]              # Target
-alfa = 0.2                      # Taxa de aprendizagem
+alfa = 0.01                     # Taxa de aprendizagem
 teta = 0                        # Limiar de ativação
 # Criterios de parada
 erroAceitavel = 0.01            # Flag de erro da rede neural
@@ -24,34 +24,43 @@ tolerancia = 0.0001             # Ajuste maximo admissivel dos pesos
 
 # Inicialização dos pesos
 Wanterior = []
-for i in range(2):
+
+for i in range (0,1):
     Wanterior.append(uniform(-0.5,0.5))    # Vetor de 1 linha e duas colunas
+    
 Banterior = uniform(-0.5, 0.5)             # Um escalar
-Yin = 0.0
+Yin = 0.0                                  # Saída
 Wnovo = Wanterior
 Bnovo = Banterior
-
+matriz = [[],[]]
 ciclos = 0
 while (ciclos < cicloMax):
     ciclos = ciclos +1
     EqTotal = 0                     # Erro Quadratico total
     # Inserir padrões de treinamento
-    for padrao in range(4):
-        EqTotal = EqTotal + 0.5 * (t[padrao] - Yin)**2
+    for padrao in range(0,3):
+        EqTotal = EqTotal + (0.5 * ((t[padrao] - Yin)**2))
         # Atualização dos pesos
-        Bnovo = Banterior + alfa * (t[padrao] - Yin)
-        for k in range(2):
-            Wnovo[k] = Wanterior[k] + alfa * x[padrao][k] * (t[padrao]- Yin)
-            Yin = Banterior + x[padrao][k]*Wanterior[k]
+        Bnovo = Banterior + (alfa * (t[padrao] - Yin))
+        for k in range(0,1):
+            print("sou k " + k)
+            Wnovo[k] = Wanterior[k] + (alfa * x[padrao][k] * (t[padrao]- Yin))
+            Yin = Banterior + (x[padrao][k]*Wanterior[k])
         Wanterior = Wnovo
         Banterior = Bnovo
-        
-    #pl(ciclos, EqTotal)
-    
-print(Wanterior)
-print(Banterior)
-print('CICLOS: ',ciclos)
+    matriz[0].append(ciclos)
+    matriz[1].append(EqTotal)
+    print(EqTotal)
+j=1
+for i in Wanterior:
+    print('w',j,' -> ', i)
+    j = j +1
 
+print('b -> ',Banterior)
+print('CICLOS: ',ciclos)
+plt.plot(matriz[0],matriz[1],'ro')
+plt.axis([0,100,0,2])
+plt.show()
 """
     Tarefa: Colocar a tolerancia como critério de parada
 
@@ -60,11 +69,9 @@ print('CICLOS: ',ciclos)
 
 """
 """
+    Modular em funções 
+    Usar map()
+    Usar numpy para os arrays
+    Fazer plot
 
-
-def error( ):
-
-    return 
-
-def 
 """
